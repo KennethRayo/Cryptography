bdd5bab0282289620ee8968f56e7cbb1acf04426
Kenneth Gut <kennethgut777@gmail.com>
2019-03-03 09:40:04 -0600

    Uploading to the Repository

---
+++ src/aes_256_encryption_decryption/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aes_256_encryption_decryption;
+
+import Layer_Presentation.JFEncrypt;
+
+/**
+ *
+ * @author KGR
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFEncrypt vistaL =  new JFEncrypt();
+        vistaL.setVisible(true);
+        vistaL.setLocationRelativeTo(null);
+    }
+    
+}
